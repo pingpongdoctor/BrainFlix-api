31ee199f7182e129340328b49e87845a6dc42daa
Simon Tran <thanhnhantran1501@gmail.com>
2023-05-26 21:29:58 -0700

    set header for cors

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ const cors = require("cors");
 const videos = require("./routes/videos.js");
 const PORT = process.env.PORT || 8000;
 
-app.use(
-  cors({
-    origin: process.CLIENT_URL,
-  })
-);
+app.use(cors());
+// Enable CORS for specific origins
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://brainflix-platform.netlify.app"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 app.use(express.json());
 app.set("trust proxy", 1);
 
